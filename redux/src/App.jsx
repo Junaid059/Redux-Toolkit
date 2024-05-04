47aba8793053ab12698ad8cf792a95fe1adfb0d6
junaid khalid <junaidkhalid2001@gmail.com>
2024-05-05 01:22:20 +0500

    initial commit

---
+++ redux/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Learn React Redux Toolkit</h1>
+      <AddTodo></AddTodo>
+      <Todos></Todos>
+    </>
+  )
+}
+
+export default App
